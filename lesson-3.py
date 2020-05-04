8cadfd13d146930d27132d0aa1de2cb1c6b8e320
gmm <m.m.gershevich@ya.ru>
2020-05-04 18:38:08 +0900

    3

--- lesson-3.py
+++ lesson-3.py
@@ -3,22 +3,21 @@
 print()
 str = '!!!Погружение в IT-профессию!!! подразумевает постоянный контакт с изучаемыми технологиями, выполнение практических заданий и общение с наставником. Для этого мы создали собственную среду обучения.'
 
-str = str.replace(',', '').replace('.', '').replace(';','').replace('—','').replace('«','').replace('»','').replace('?','').replace('!','').replace('(','').replace(')','')
-print(str)
+# str = str.replace(',', '').replace('.', '').replace(';','').replace('—','').replace('«','').replace('»','').replace('?','').replace('!','').replace('(','').replace(')','')
+# print(str)
 
-print()
 #################
 
 #2 сформировать list со словами (split);
 
 #################
-# x_srt=re.split(' ', x_srt)
-# print(x_srt)
+# answer2 = str.split()
+# print(answer2)
 #################
 #3 привести все слова к нижнему регистру (map);
 #################
-# new_srt=list(map(lambda x: x.lower(), x_srt))
-# print(new_srt)
+# answer3 = list(map(lambda x: x.lower(), answer2))
+# print(answer3)
 #################
 #4 получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте;
 #################
